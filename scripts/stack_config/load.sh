a91cac8ac188e155cda89c4321f0dc31916a18fc
codeface-io <sebastian@nohype.ai>
2026-04-02 22:43:34 +0200

    Simplify config by removing custom path

--- scripts/stack_config/load.sh
+++ scripts/stack_config/load.sh
@@ -30,7 +30,6 @@ GIT_USER_NAME=$($JQ -r '.git.user_name' "$CONFIG")
 GIT_USER_EMAIL=$($JQ -r '.git.user_email' "$CONFIG")
 GIT_CORE_EDITOR=$($JQ -r '.git.core_editor' "$CONFIG")
 GIT_REPOS_FOLDER=$($JQ -r '.repos.folder' "$CONFIG")
-GIT_REPOS_FOLDER_TEMPLATE=$($JQ -r '.repos.folder_template' "$CONFIG")
 SKIP_BREW_PACKAGE_UPDATES=$($JQ -r '.flags.skip_brew_updates' "$CONFIG")
 RESTORE_IDE_SETTINGS=$($JQ -r '.flags.restore_ide_settings' "$CONFIG")
 set +a # Turn off auto-export
